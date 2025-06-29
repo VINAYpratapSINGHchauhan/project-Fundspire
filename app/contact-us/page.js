ed586758fbf4deea0cd36ee3af1c428eb23e0c5c
Vinay Pratap Singh Chauhan <145904896+VINAYpratapSINGHchauhan@users.noreply.github.com>
2025-06-29 17:01:09 +0530

    added metadata

--- app/contact-us/page.js
+++ app/contact-us/page.js
@@ -9,3 +9,7 @@ const page = () => {
 }
 
 export default page
+export const metadata = {
+  title: "Contact Us - Fundspire",
+  description: "Have questions or need support? Reach out to the Fundspire team through our contact form, email, or social media.",
+};
