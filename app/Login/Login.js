fcd5174c92353cb6443e0773e6afddd101bac677
Vinay Pratap Singh Chauhan <145904896+VINAYpratapSINGHchauhan@users.noreply.github.com>
2025-08-30 17:33:00 +0530

    updation for authentication and backend mongoDB setup

--- app/Login/Login.js
+++ app/Login/Login.js
@@ -19,7 +19,7 @@ const Login = () => {
             <h1 className='text-center font-bold text-3xl '>Login to Get Started</h1>
 
             <div className="flex flex-col gap-2  items-center  p-10">
-                <button
+                <button  onClick={() => { signIn("google") }}
                     className="flex items-center w-64 bg-slate-50 text-black border border-gray-300 rounded-lg shadow-md max-w-xs px-6 py-2 text-sm font-medium  hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
                     <svg className="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
                         viewBox="-0.5 0 48 48" version="1.1">
@@ -45,7 +45,7 @@ const Login = () => {
                     </svg>
                     <span>Continue with Google</span>
                 </button>
-                <button
+                {/* <button
                     className="flex items-center w-64 bg-slate-50 text-black border border-gray-300 rounded-lg shadow-md max-w-xs px-6 py-2 text-sm font-medium  hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
                     <svg className="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
                         viewBox="0 -2 44 44" version="1.1">
@@ -93,7 +93,7 @@ const Login = () => {
                     </svg>
 
                     <span>Continue with Facebook</span>
-                </button>
+                </button> */}
                 <button onClick={() => { signIn("github") }}
                     className="flex items-center w-64 bg-slate-50 text-black border border-gray-300 rounded-lg shadow-md max-w-xs px-6 py-2 text-sm font-medium  hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
                     <svg className="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
